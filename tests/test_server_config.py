06fc324ab252b18e8e797079f32d67e7050cfa69
Trevor <rowdeil@gmail.com>
2025-03-17 05:41:33 -0700

    pathed correct urls for test_route_registration

--- tests/test_server_config.py
+++ tests/test_server_config.py
@@ -69,8 +69,8 @@ def test_route_registration():
         assert url_for('home.index') is not None
         assert url_for('auth.login') is not None
         assert url_for('auth.register_plumber') is not None
-        assert url_for('auth.logout') is not None
-        assert url_for('auth.reset_password') is not None
+        assert url_for('auth.logout_route') is not None
+        assert url_for('auth.reset_password_route') is not None
         assert url_for('auth.api_login') is not None
         assert url_for('auth.api_register_plumber') is not None
 
